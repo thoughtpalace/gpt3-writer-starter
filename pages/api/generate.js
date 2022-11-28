a76b499446c7968cd595a493ac72fc134a18df83
dev <dev@devs-MacBook-Pro.local>
2022-11-28 11:09:08 -0500

    new stuff

---
+++ pages/api/generate.js
@@ -0,0 +1,80 @@
+ import { Configuration, OpenAIApi } from 'openai';
+
+ const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+ });
+
+const openai = new OpenAIApi(configuration)
+
+// const basePromptPrefix = `Give me a long funny response to my enemies comments below and make it sound like kevin hart clap back and use curse words and make sure it sounds black:
+
+// `
+
+// const generateAction = async (req, res) => {
+//   // Run first prompt
+//   console.log(`API: ${basePromptPrefix}\n ${req.body.userInput}\n response:`)
+
+//   const baseCompletion = await openai.createCompletion({
+//     model: 'text-davinci-002',
+//     prompt: `${basePromptPrefix}${req.body.userInput}`,
+//     temperature: 0.73,
+//     max_tokens: 1000,
+//   });
+  
+//   const basePromptOutput = baseCompletion.data.choices.pop();
+
+//   res.status(200).json({ output: basePromptOutput });
+// };
+
+// export default generateAction;
+
+
+const basePromptPrefix =
+`
+Write me a detailed funny dirty joke in response to the text below like kevin hart clapping back in detail and hilarious.
+
+text:
+`
+
+const generateAction = async (req, res) => {
+  console.log(`API: ${basePromptPrefix}${req.body.userInput}`)
+
+  const baseCompletion = await openai.createCompletion({
+    model: 'text-davinci-002',
+    prompt: `${basePromptPrefix}${req.body.userInput}`,
+    temperature: 0.8,
+    max_tokens: 800,
+  });
+  
+  const basePromptOutput = baseCompletion.data.choices.pop();
+
+  // I build Prompt #2.
+  const secondPrompt = 
+  `
+  Take the text of content and output  below and generate a response written in the style of Kevin Hart and Eddie Murphy. Make it feel like a funny story. Don't just list the points. Go deep into each one. Explain why using curse words.
+
+  content: ${req.body.userInput}
+
+  output : ${basePromptOutput.text}
+
+  response:
+  `
+  
+  // I call the OpenAI API a second time with Prompt #2
+  const secondPromptCompletion = await openai.createCompletion({
+    model: 'text-davinci-002',
+    prompt: `${secondPrompt}`,
+    // I set a higher temperature for this one. Up to you!
+    temperature: 0.85,
+		// I also increase max_tokens.
+    max_tokens: 1250,
+  });
+  
+  // Get the output
+  const secondPromptOutput = secondPromptCompletion.data.choices.pop();
+
+  // Send over the Prompt #2's output to our UI instead of Prompt #1's.
+  res.status(200).json({ output: secondPromptOutput });
+};
+
+export default generateAction;
--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,84 @@ import Head from 'next/head';
 import Image from 'next/image';
 import buildspaceLogo from '../assets/buildspace-logo.png';
 
+import { useState } from 'react';
+
+
 const Home = () => {
+  const [userInput, setUserInput] = useState('');
+
+  const [apiOutput, setApiOutput] = useState('')
+  const [isGenerating, setIsGenerating] = useState(false)
+
+  const callGenerateEndpoint = async () => {
+  setIsGenerating(true);
+  
+  console.log("Calling OpenAI...")
+  const response = await fetch('/api/generate', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ userInput }),
+  });
+
+  const data = await response.json();
+  const { output } = data;
+  console.log("OpenAI replied...", output.text)
+
+  setApiOutput(`${output.text}`);
+  setIsGenerating(false);
+}
+
+  const onUserChangedText = (event) => {
+    setUserInput(event.target.value);
+  };
+
   return (
     <div className="root">
       <Head>
-        <title>GPT-3 Writer | buildspace</title>
+        <title>Clap Back!</title>
       </Head>
       <div className="container">
         <div className="header">
           <div className="header-title">
-            <h1>sup, insert your headline here</h1>
+            <h1> Clap Back!  </h1> 
           </div>
           <div className="header-subtitle">
-            <h2>insert your subtitle here</h2>
+            <h2> Enter all the text from your worst enemey and make the response you always dreamed of </h2>
           </div>
         </div>
+        <div className="prompt-container">
+          <textarea 
+          className="prompt-box"
+          placeholder="start typing what they said here"
+          value={userInput}
+          onChange={onUserChangedText}
+          
+          />
+        </div>
+        <div className="prompt-buttons">
+    <a className="generate-button" onClick={callGenerateEndpoint}>
+      <div className="generate">
+        {isGenerating ? <span class="loader"></span> : <p>Generate</p>}
+      </div>
+    </a>
+  </div>
+
+  {apiOutput && (
+  <div className="output">
+    <div className="output-header-container">
+      <div className="output-header">
+        <h3>Output</h3>
+      </div>
+    </div>
+    <div className="output-content">
+      <p>{apiOutput}</p>
+    </div>
+  </div>
+)}
+
+  
       </div>
       <div className="badge-container grow">
         <a
@@ -26,7 +89,7 @@ const Home = () => {
         >
           <div className="badge">
             <Image src={buildspaceLogo} alt="buildspace logo" />
-            <p>build with buildspace</p>
+            <p>built with hate for your punk ass enemies</p>
           </div>
         </a>
       </div>
